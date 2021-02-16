291bede7ed75ad361a16987de2e24b4bdeed6e27
paraparata <srizalmp@gmail.com>
2021-02-17 03:50:03 +0700

    add about menu

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ function About({ show, onClose }) {
           <div className="w-full mx-auto my-4">
             <div className="about w-full h-72 ss:h-36 px-4 py-2 flex flex-col rounded border-2 border-yellow-300 text-gray-700 overflow-y-scroll">
               <figure className="mb-4">
-                <blockquote cite="#">
+                <blockquote cite="https://github.com/paraparata/parakamon">
                   <p>Parakamon is para pokemon!</p>
                 </blockquote>
                 <figcaption>
@@ -78,7 +78,7 @@ function About({ show, onClose }) {
               <div className="mt-12 text-center">
                 <hr />
                 <p className="mt-2">
-                  Made with wuff by{" "}
+                  Made with React + Graphql + Apollo + wuff by{" "}
                   <a
                     href="https://paraparata.github.io/"
                     title="Paraparata"
@@ -90,7 +90,7 @@ function About({ show, onClose }) {
                   <br />
                   in{" "}
                   <a
-                    href="#"
+                    href="https://github.com/paraparata/parakamon"
                     title="parakamon repository"
                     target="_blank"
                     rel="noreferrer"
